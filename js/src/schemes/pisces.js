e48baa7d939084e008cb54ca304866350fbfb794
Leon <fe.lizhoukai@gmail.com>
2016-12-08 22:25:37 +0800

    Site updated: 2016-12-08 22:25:37

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
   var duoshuoQuery = {short_name:"vuepack"};
   var ds = document.createElement('script');
   ds.type = 'text/javascript';ds.async = true;
-  ds.src = 'https://ohv0hyr4v.qnssl.com/embed.js';
+  ds.src = 'https://ohv0hyr4v.qnssl.com/embedd.js';
   ds.charset = 'UTF-8';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
 });
