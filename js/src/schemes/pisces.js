a16d5e1d4afe2248a6d189fafec7459b15a3ad70
Leon <fe.lizhoukai@gmail.com>
2016-12-08 17:48:06 +0800

    Site updated: 2016-12-08 17:48:06

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -29,13 +29,13 @@ $(document).ready(function () {
 
   //跳转到首页
   $('#sidebar .circle').click(function(){
-    window.location.href= 'http://likr.cn/'
+    window.location.href= 'https://likr.cn/'
   })
 
   var duoshuoQuery = {short_name:"vuepack"};
   var ds = document.createElement('script');
   ds.type = 'text/javascript';ds.async = true;
-  ds.src = 'http://static.duoshuo.com/embed.js';
+  ds.src = 'https://static.duoshuo.com/embed.js';
   ds.charset = 'UTF-8';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
 });
